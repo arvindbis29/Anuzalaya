ffc3d632a976e2f7cd1288afbe3485ba84ef8a68
Arvind Bishnoi <19107@iiitu.ac.in>
2021-11-20 20:04:57 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,16 @@
+
+import tkinter as tk
+  
+root = tk.Tk()
+
+  
+
+  
+# Frame for button border with black border color
+
+bttn = tk.Button( text = 'Submit', fg = 'black',
+                 bg = 'yellow',font = (("Times New Roman"),15))
+bttn.pack()
+
+  
+root.mainloop()
